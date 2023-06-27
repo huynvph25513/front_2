4ffce2e16831d8b4a454cc2c25b406b5119ac015
Nguyen Van Huy <huynvph25513@fpt.edu.vn>
2023-06-27 13:40:27 +0700

    component

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+import Hello from "./components/Hello";
+
+type AppState = {
+  name: string;
+  children: { id: number; name: string }[];
+};
+
+function App() {
+  const [info] = useState<AppState>({
+    name: "Dat",
+    children: [
+      { id: 1, name: "Thanh Tung" },
+      { id: 2, name: "Tung Lam" },
+    ],
+  });
+  return (
+    <>
+      <Hello name="Son" age={20} info={info} />
+    </>
+  );
+}
+
+export default App;
