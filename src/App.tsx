fbe84e56882603609cd9d0c90cf5e69ae99d64bb
Nguyen Van Huy <huynvph25513@fpt.edu.vn>
2023-07-01 12:29:47 +0700

    Buổi 2: Add, Remove

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,24 @@ const carsData = [
   { id: 1, name: "BMW" },
   { id: 2, name: "Mercedes" },
   { id: 3, name: "Audi" },
-]
+];
 
 function App() {
   const [cars, setCars] = useState<ICar[]>(carsData);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<null>(null);
 
-  const addCar = (car: ICar) => { };
-  const removeCar = (id: number | string) => { };
-  const listCar = () => { };
+  const addCar = (car: ICar) => {
+    setCars([...cars, car]);
+  };
+  const removeCar = (id: number | string) => {
+    setCars(cars.filter((car) => car.id !== id));
+  };
   return (
     <>
       <div className="w-96 mx-auto border border-gray-500 p-2">
-        <Form />
-        <List data={cars} />
+        <Form onAdd={addCar} />
+        <List data={cars} onRemove={removeCar} />
       </div>
     </>
   );
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,21 +3,23 @@ import React from "react";
 type ButtonProps = {
     type?: "primary" | "danger";
     loading?: boolean;
-    shape?: "round" | "cricle" | "default";
+    shape?: "round" | "circle" | "default";
+    onClick?: () => void;
     icon?: React.ReactNode;
     children?: React.ReactNode;
 };
 
-const Button = ({ icon, type, loading, shape, children }: ButtonProps) => {
+const Button = ({ icon, type, loading, shape, children, onClick }: ButtonProps) => {
     return (
         <button
             className={`border border-gray-500 py-2 px-4
             ${type === "primary" && "text-white bg-blue-500"}
             ${type === "danger" && "text-white bg-red-500"}
             ${shape === "round" && "rounded-full"}
-            ${shape === "cricle" && "rounded-full w-10 h-10"}
+            ${shape === "circle" && "rounded-full w-10 h-10"}
             ${shape === "default" && "rounded-md"}
-            `}
+    `}
+            onClick={onClick}
         >
             {icon && icon}
             {children}
@@ -25,4 +27,4 @@ const Button = ({ icon, type, loading, shape, children }: ButtonProps) => {
     );
 };
 
-export default Button
+export default Button;
